4246cb4142f65852367ff637fd4b0e572f730790
august-zou <zjzouxu@gmail.com>
2014-05-19 15:08:25 +0800

    first ci

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearnConfigCap::Application.config.secret_key_base = '761513d6601498f7df8198db4ac732b3b1d24b4024f6375daa2281c70bc132cbdb151c4b163da573cd8b72455f2b6ac115cba8411c3089dad48f112d35a2ad6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LearnConfigCap::Application.config.session_store :cookie_store, key: '_learn-config-cap_session'
